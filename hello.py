6927f2ba7fd8cc1ea58ef6627a5896b4d18a2178
Kate Larkins <larkinskatharine@gmail.com>
2025-03-06 16:39:06 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 print("Hello Pico World")
 
 # simplist Blinky program
-from machine import Pin
-import time
-pin = PIN("LED", Pin.Out)
+# from machine import Pin
+# import time
+# pin = PIN("LED", Pin.Out)
 
+# while True:
+#     pin.value(1)
+#     time.sleep(1)
+#     pin.value(0)
+#     time.sleep(1)
+
+from machine import Pin
+from utime import sleep
+
+pin = Pin("LED", Pin.OUT)
+
+print("LED starts flashing...")
 while True:
-    pin.value(1)
-    time.sleep(1)
-    pin.value(0)
-    time.sleep(1)
+    try:
+        pin.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+pin.off()
+print("Finished.")
 
 # Flash LED
 # from machine import Pin
